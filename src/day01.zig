e397e6bea978edb638d5062ee30350cc9d2b5849
Walther Chen <walther.chen@gmail.com>
2021-12-01 10:29:15 -0500

    day01 no alloc

--- src/day01.zig
+++ src/day01.zig
@@ -1,54 +1,40 @@
+// previously allocated.
+// This solution follows https://github.com/SpexGuy/Advent2021/blob/main/src/day01.zig,
+// which uses an array as the window instead of separate vars.
+
 const std = @import("std");
 const data = @embedFile("../input/day01.txt");
 
 pub fn main() anyerror!void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    var allocator = &arena.allocator;
+    var part01: usize = 0;
+    var part02: usize = 0;
+    var count: usize = 0;
+    var window = [3]i64{ 0, 0, 0 };
 
-    var input = std.ArrayList(i64).init(allocator);
     var lines = std.mem.tokenize(u8, data, "\n");
+
     while (lines.next()) |line| {
         const x = try std.fmt.parseInt(i64, line, 10);
-        try input.append(x);
-    }
-    const part01_output = part01(input.items);
-    const part02_output = part02(input.items);
 
-    std.log.info("part 01: {d}", .{part01_output});
-    std.log.info("part 02: {d}", .{part02_output});
-}
-
-fn part01(input: []const i64) usize {
-    var increased: usize = 0;
-
-    // window of 2
-    var i: usize = 1;
-    while (i < input.len) : (i += 1) {
-        if (input[i] > input[i - 1]) {
-            increased += 1;
+        // part01, compare to leading edge of window (like
+        // window of 2, including the current x)
+        if (count >= 1 and x > window[2]) {
+            part01 += 1;
         }
-    }
 
-    return increased;
-}
-
-fn part02(input: []const i64) usize {
-    var increased: usize = 0;
-    var prev_window_sum: i64 = 0;
-
-    // window of 3
-    var i: usize = 2;
-    while (i < input.len) : (i += 1) {
-        const sum = input[i] + input[i - 1] + input[i - 2];
-        if (sum > prev_window_sum) {
-            increased += 1;
+        // part02, compare to trailing edge of window (like
+        // window of 4, including the current x) (which is
+        // like comparing the sums of two windows(3))
+        if (count >= 3 and x > window[0]) {
+            part02 += 1;
         }
-        prev_window_sum = sum;
+
+        window[0] = window[1];
+        window[1] = window[2];
+        window[2] = x;
+
+        count += 1;
     }
 
-    // subtract 1 because first comparison is always an increase
-    increased -= 1;
-
-    return increased;
+    std.log.info("part 01: {d}, part 02: {d}", .{ part01, part02 });
 }
