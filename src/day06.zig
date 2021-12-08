dec564e7799d9e9302f27a05139042a67ce02945
Walther Chen <walther.chen@gmail.com>
2021-12-08 10:25:51 -0500

    day06: part 2, shift in array instead of using ArrayList

--- src/day06.zig
+++ src/day06.zig
@@ -7,45 +7,50 @@ const data = @embedFile("../input/day06.txt");
 // from prompt
 //const data = "3,4,3,1,2";
 
-const DAYS = 80;
+const PART_1_DAYS = 80;
+const PART_2_DAYS = 256;
 
 // required to print if release-fast
 pub const log_level: std.log.Level = .info;
 
 pub fn main() anyerror!void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const alloc = &arena.allocator;
-
-    var fishes = ArrayList(u4).init(alloc);
+    var fishes = [_]usize{ 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
     var it = std.mem.tokenize(u8, std.mem.trim(u8, data, "\r\n"), ",");
     while (it.next()) |fish| {
-        std.debug.print("{c}", .{fish});
-        try fishes.append(try parseInt(u4, fish, 10));
+        fishes[try parseInt(usize, fish, 10)] += 1;
     }
 
-    // part01
     var i: usize = 0;
-    while (i < DAYS) : (i += 1) {
-        var new_fishes: usize = 0;
-        for (fishes.items) |*fish| {
-            switch (fish.*) {
-                0 => {
-                    fish.* = 6;
-                    new_fishes += 1;
-                },
-                1...8 => fish.* -= 1,
-                else => unreachable,
-            }
+    while (i < PART_2_DAYS) : (i += 1) {
+        if (i == PART_1_DAYS) {
+            std.log.info("part01: {d}", .{count(fishes)});
         }
 
-        // append new fishes
-        var j: usize = 0;
-        while (j < new_fishes) : (j += 1) {
-            try fishes.append(8);
-        }
+        shift_agg(&fishes);
     }
 
-    std.log.info("part01: {}", .{fishes.items.len});
+    std.log.info("part02: {}", .{count(fishes)});
+}
+
+fn count(fishes: [9]usize) usize {
+    var res: usize = 0;
+    for (fishes) |fish_count| {
+        res += fish_count;
+    }
+
+    return res;
+}
+
+fn shift_agg(fishes: *[9]usize) void {
+    var new_fishes = fishes[0];
+    fishes[0] = fishes[1];
+    fishes[1] = fishes[2];
+    fishes[2] = fishes[3];
+    fishes[3] = fishes[4];
+    fishes[4] = fishes[5];
+    fishes[5] = fishes[6];
+    fishes[6] = fishes[7] + new_fishes;
+    fishes[7] = fishes[8];
+    fishes[8] = new_fishes;
 }
