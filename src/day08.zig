4b053e562e952766b98e17f5831445d9aa1b21ff
Walther Chen <walther.chen@gmail.com>
2021-12-09 15:06:49 -0500

    day08: part1

---
+++ src/day08.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const data = @embedFile("../input/day08.txt");
+
+// from prompt
+//const data =
+//    \\be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+//    \\edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+//    \\fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+//    \\fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+//    \\aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+//    \\fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+//    \\dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+//    \\bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+//    \\egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+//    \\gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+//;
+
+// required to print if release-fast
+pub const log_level: std.log.Level = .info;
+
+pub fn main() anyerror!void {
+    var part1: usize = 0;
+
+    var lines = std.mem.tokenize(u8, data, "\n");
+    while (lines.next()) |line| {
+        var entry = std.mem.split(u8, line, " | ");
+        _ = entry.next().?;
+        var four_digits = std.mem.tokenize(u8, entry.next().?, " ");
+        while (four_digits.next()) |digit| {
+            switch (digit.len) {
+                2, 3, 4, 7 => part1 += 1,
+                else => {},
+            }
+        }
+    }
+    std.log.info("part1: {}", .{part1});
+}
