ad7b8370d162b5861cb03ce416b34ef04ebc2bf0
Walther Chen <walther.chen@gmail.com>
2021-12-12 16:07:50 -0500

    day10: part1

---
+++ src/day10.zig
@@ -0,0 +1,79 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+const parseInt = std.fmt.parseInt;
+const asc_usize = std.sort.asc(usize);
+
+const data = @embedFile("../input/day10.txt");
+
+// from prompt
+//const data =
+//    \\[({(<(())[]>[[{[]{<()<>>
+//    \\[(()[<>])]({[<{<<[]>>(
+//    \\{([(<{}[<>[]}>{[]{[(<()>
+//    \\(((({<>}<{<{<>}{[]{[]{}
+//    \\[[<[([]))<([[{}[[()]]]
+//    \\[{[{({}]{}}([{[{{{}}([]
+//    \\{<[[]]>}<{[{[{[]{()[[[]
+//    \\[<(<(<(<{}))><([]([]()
+//    \\<{([([[(<>()){}]>(<<{{
+//    \\<{([{{}}[<[[[<>{}]]]>[]]
+//;
+
+// required to print if release-fast
+pub const log_level: std.log.Level = .info;
+
+pub fn main() anyerror!void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const alloc = &arena.allocator;
+
+    var part1: usize = 0;
+    var stack = ArrayList(u8).init(alloc);
+
+    var lines = std.mem.tokenize(u8, data, "\n");
+    while (lines.next()) |line| {
+        for (line) |c| {
+            switch (c) {
+                '(', '[', '{', '<' => {
+                    try stack.append(c);
+                },
+                else => {
+                    // the break ignores mismatch in number of parens
+                    const b = stack.popOrNull() orelse break;
+                    switch (b) {
+                        '(' => if (c != ')') {
+                            part1 += score(c);
+                            break;
+                        },
+                        '[' => if (c != ']') {
+                            part1 += score(c);
+                            break;
+                        },
+                        '{' => if (c != '}') {
+                            part1 += score(c);
+                            break;
+                        },
+                        '<' => if (c != '>') {
+                            part1 += score(c);
+                            break;
+                        },
+                        else => unreachable,
+                    }
+                },
+            }
+        }
+    }
+
+    std.log.info("part1: {}", .{part1});
+}
+
+fn score(c: u8) usize {
+    return switch (c) {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        else => unreachable,
+    };
+}
