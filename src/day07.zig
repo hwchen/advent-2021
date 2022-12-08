6f7d0a27f18ad66bf2167352af347f7fe4992989
Walther Chen <walther.chen@gmail.com>
2022-12-08 00:19:09 -0500

    day07: update

--- src/day07.zig
+++ src/day07.zig
@@ -6,9 +6,6 @@ const asc_usize = std.sort.asc(usize);
 
 const data = @embedFile("input/day07.txt");
 
-// from prompt
-//const data = "16,1,2,0,4,2,7,1,2,14";
-
 // required to print if release-fast
 pub const log_level: std.log.Level = .info;
 
@@ -17,23 +14,38 @@ pub fn main() anyerror!void {
     defer arena.deinit();
     const alloc = arena.allocator();
 
+    const out = try run(data, alloc);
+
+    std.log.info("part01: {d}, part02: {d}", .{ out.part01, out.part02 });
+}
+
+fn run(input: []const u8, alloc: Allocator) !struct { part01: u64, part02: u64 } {
     var crabs = blk: {
         var res = ArrayList(usize).init(alloc);
 
-        var it = std.mem.tokenize(u8, std.mem.trim(u8, data, "\r\n"), ",");
+        var it = std.mem.tokenize(u8, std.mem.trim(u8, input, "\r\n"), ",");
         while (it.next()) |crab| {
             try res.append(try parseInt(usize, crab, 10));
         }
         std.sort.sort(usize, res.items, {}, asc_usize);
         break :blk try res.toOwnedSlice();
     };
+    defer alloc.free(crabs);
 
     // initially tried avg, but it's actually median
     const median: isize = @intCast(isize, crabs[crabs.len / 2]);
-    var part1: isize = 0;
+    var part01: isize = 0;
     for (crabs) |crab| {
-        part1 += try std.math.absInt(@intCast(isize, crab) - median);
+        part01 += try std.math.absInt(@intCast(isize, crab) - median);
     }
 
-    std.log.info("part1: {}", .{part1});
+    return .{ .part01 = @intCast(u64, (part01)), .part02 = 0 };
+}
+
+test "test_day07" {
+    const input = "16,1,2,0,4,2,7,1,2,14";
+
+    const out = try run(input, std.testing.allocator);
+    try std.testing.expectEqual(out.part01, 37);
+    try std.testing.expectEqual(out.part02, 0);
 }
